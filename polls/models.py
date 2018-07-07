10d9cd00d7688b8b741e5d8dfb626b61fc7a8eb2
sachin <sachinjangid021@gmail.com>
2018-07-07 22:16:52 +0530

    migrating models

--- polls/models.py
+++ polls/models.py
@@ -7,6 +7,6 @@ class Question(models.Model):
 	publish_date = models.DateTimeField('date published')
 
 class Choice(models.Model):
-	question = Model.ForeignKey(Question, on_delete=models.CASCADE)
+	question = models.ForeignKey(Question, on_delete=models.CASCADE)
 	choice_text = models.CharField(max_length=200)
 	votes = models.IntegerField(default=0)
